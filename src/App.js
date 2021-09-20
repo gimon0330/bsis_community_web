6e5808f5fbb983771ff9dc5ba92967c8012a8ed2
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2021-09-20 12:47:00 +0900

    로그인 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Navibar, Footer } from './components';
-import { Home, info, community_all, community_class, DM, NotFound, myinfo } from './pages';
+import { Home, info, community_all, community_class, DM, NotFound, myinfo, login } from './pages';
 import "./App.css"
 
 export default class App extends Component {
@@ -16,6 +16,7 @@ export default class App extends Component {
             <Route exact path="/community/all" component={community_all}/>
             <Route exact path="/community/class" component={community_class}/>
             <Route exact path="/myinfo" component={myinfo}/>
+            <Route exact path="/login" component={login}/>
 
             <Route component={NotFound}/>
           </Switch>
